d0f3b989ad0f3cd884b38afa8d3dd92846d10219
Ciaran Agnew <2762322A@student.gla.ac.uk>
2024-03-10 23:49:19 +0000

    Before 15 tasks

--- rango/urls.py
+++ rango/urls.py
@@ -4,14 +4,26 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('category/<slug:category_name_slug>/', views.show_category, name ='show_category'),
-    path('add_category/', views.add_category, name='add_category'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('category/<slug:category_name_slug>/', views.ShowCategoryView.as_view(), name='show_category'),
+    path('add_category/', views.AddCategoryView.as_view(), name='add_category'),
+    path('restricted/', views.RestrictedView.as_view(), name='restricted'),
+    path('register_profile/', views.RegisterProfileView.as_view(), name="register_profile"),
+    path('goto/', views.GotoView.as_view(), name='goto'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
-    path('restricted/', views.restricted, name='restricted'),
-    path('search/', views.search, name='search'),
+    path('profile/<username>/', views.ProfileView.as_view(), name='profile'),
+    path('profiles/', views.ListProfilesView.as_view(), name='list_profiles'),
+
+]
+
+
+
+    #path('search/', views.search, name='search'),
     #path('logout/', views.user_logout, name='logout'),
     #path('register/', views.register, name='register'),
     #path('login/', views.user_login, name='login'),
-]
+    #path('about/', views.about, name='about'),
+    #path('add_category/', views.add_category, name='add_category'),
+    #path('', views.index, name='index'),
+    #path('category/<slug:category_name_slug>/', views.show_category, name ='show_category'),
